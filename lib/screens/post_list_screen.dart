c6d9d688644a07aec5d1fbba8cb79763d9f2fe9c
Abdurrahman <abdurrahmanms5258@gmail.com>
2025-02-16 11:15:05 +0530

    added search option

--- lib/screens/post_list_screen.dart
+++ lib/screens/post_list_screen.dart
@@ -4,82 +4,118 @@ import '../providers/post_provider.dart';
 import '../widgets/post_form.dart';
 import '../models/post.dart';
 
-class PostListScreen extends StatelessWidget {
+class PostListScreen extends StatefulWidget {
   const PostListScreen({super.key});
+
+  @override
+  _PostListScreenState createState() => _PostListScreenState();
+}
+
+class _PostListScreenState extends State<PostListScreen> {
+  TextEditingController searchController = TextEditingController();
+  String searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
     final postProvider = Provider.of<PostProvider>(context);
+
+    List<Post> filteredPosts = postProvider.posts
+        .where((post) =>
+            post.title!.toLowerCase().contains(searchQuery.toLowerCase()))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Posts'),
         backgroundColor: Colors.green,
       ),
-      body: postProvider.posts.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.separated(
-              itemCount: postProvider.posts.length,
-              separatorBuilder: (context, index) => const SizedBox(height: 10),
-              padding: const EdgeInsets.all(25),
-              itemBuilder: (context, index) {
-                final post = postProvider.posts[index];
-                return Card(
-                  elevation: 6,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          post.title ?? 'No Title',
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          post.body ?? 'No Content',
-                          style:
-                              TextStyle(fontSize: 16, color: Colors.grey[700]),
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.edit,
-                                  color: Colors.blueAccent),
-                              onPressed: () =>
-                                  _openPostForm(context, postProvider, post),
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete, color: Colors.red),
-                              onPressed: () async {
-                                bool? confirmDelete =
-                                    await _showDeleteConfirmationDialog(
-                                        context);
-                                if (confirmDelete == true) {
-                                  postProvider.deletePost(context, post.id!);
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search by title...',
+                prefixIcon: const Icon(Icons.search),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  searchQuery = value;
+                });
               },
             ),
+          ),
+          Expanded(
+            child: filteredPosts.isEmpty
+                ? const Center(child: Text("No posts found"))
+                : ListView.separated(
+                    itemCount: filteredPosts.length,
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 10),
+                    padding: const EdgeInsets.all(10),
+                    itemBuilder: (context, index) {
+                      final post = filteredPosts[index];
+                      return Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                post.title ?? 'No Title',
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                post.body ?? 'No Content',
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.grey[700]),
+                              ),
+                              const SizedBox(height: 8),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.edit,
+                                        color: Colors.blueAccent),
+                                    onPressed: () => _openPostForm(
+                                        context, postProvider, post),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.delete,
+                                        color: Colors.red),
+                                    onPressed: () async {
+                                      bool? confirmDelete =
+                                          await _showDeleteConfirmationDialog(
+                                              context);
+                                      if (confirmDelete == true) {
+                                        postProvider.deletePost(
+                                            context, post.id!);
+                                      }
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.green,
-        child: const Icon(
-          Icons.add,
-          color: Colors.black,
-        ),
+        child: const Icon(Icons.add, color: Colors.black),
         onPressed: () => _openPostForm(context, postProvider, null),
       ),
     );
